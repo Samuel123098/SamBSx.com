eed4a3bea52c7aecd25d4d48c956e6caede0c2e7
Samuel123098 <S4muel2018@hotmail.com>
2025-06-01 02:26:14 +0100

    fixed scaling

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,33 @@ import './App.css'
 function App() {
   const zoomamount = 0
 
+  const elements = document.querySelectorAll('.AfterWards');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+
+        console.log('Element is visible:', entry.target);
+
+      } else {
+
+        console.log('Element is NOT visible:', entry.target);
+        
+      }
+    });
+  });
+
+  elements.forEach(el => observer.observe(el));
+
   return (
     <>
       <div className='ImageZoomLength'> 
           <div className='ImageZoomHolder'>
                 <img src="./beautiful-shot-snowy-mountain-sunset.jpg" className="ImageZoomReal"></img>
           </div>
+      </div>
+      <div className='AfterWards'>
+
       </div>
     </>
   )
